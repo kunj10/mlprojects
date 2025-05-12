87d7671bffb72fe1238631982902a42f910d602e
kunj10 <kunjpatel91012@gmail.com>
2025-05-12 22:01:12 +0530

    logging

--- src/exception.py
+++ src/exception.py
@@ -1,11 +1,12 @@
 import sys
+from src.logger import logging 
 
 def error_msg_detail(error,error_detail:sys):
   _,_,exc_tb = error_detail.exc_info()
   
   file_name = exc_tb.tb_frame.f_code.co_filename
   
-  error_msg = "Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
+  error_msg = "Error occurred in python script name [{0}] line number [{1}] error message[{2}]".format(
      file_name,exc_tb.tb_lineno,str(error))
   
   return error_msg
