b21b3b3178043de7abea9cc59039627f383a5fcf
kunj10 <kunjpatel91012@gmail.com>
2025-05-14 14:42:05 +0530

    Data transformation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-# main.py
 from src.components.data_ingestion import DataIngestion
+from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainer
 
-if __name__ == "__main__":
-    obj = DataIngestion()
-    obj.initiate_data_ingestion()
+
--- src/exception.py
+++ src/exception.py
@@ -1,22 +1,24 @@
 import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from src.logger import logging
-def error_msg_detail(error,error_detail:sys):
-  _,_,exc_tb = error_detail.exc_info()
-  
-  file_name = exc_tb.tb_frame.f_code.co_filename
-  
-  error_msg = "Error occurred in python script name [{0}] line number [{1}] error message[{2}]".format(
+
+def error_message_detail(error,error_detail:sys):
+    _,_,exc_tb=error_detail.exc_info()
+    file_name=exc_tb.tb_frame.f_code.co_filename
+    error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
      file_name,exc_tb.tb_lineno,str(error))
-  
-  return error_msg
-  
+
+    return error_message
+
+    
+
 class CustomException(Exception):
-    def __init__(self,error_msg,error_detail:sys):
-      super().__init__(error_msg)
-      self.error_msg = error_msg_detail(error_msg,error_detail=error_detail)
+    def __init__(self,error_message,error_detail:sys):
+        super().__init__(error_message)
+        self.error_message=error_message_detail(error_message,error_detail=error_detail)
     
     def __str__(self):
-      return self.error_msg
-  
+        return self.error_message
+    
+
+
+        
